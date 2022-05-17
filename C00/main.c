eabb478ffa927336003b5b5678f2943b4fadbce5
Gaeon <gaeon0413@gmail.com>
2022-05-17 15:12:57 +0900

    C00

--- C00/main.c
+++ C00/main.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
 #include <stdio.h>
+
 #include "ft_putchar/ft_putchar.c"
 #include "ft_print_alphabet/ft_print_alphabet.c"
 #include "ft_print_reverse_alphabet/ft_print_reverse_alphabet.c"
 #include "ft_print_numbers/ft_print_numbers.c"
+#include "ft_is_negative/ft_is_negative.c"
 
 int main()
 {
@@ -11,11 +13,20 @@ int main()
 
     c = 'a';
     ft_putchar(c);
-
     printf("\n");
 
     ft_print_alphabet();
+    printf("\n");
 
+    ft_print_reverse_alphabet();
+    printf("\n");
+
+    ft_print_numbers();
+    printf("\n");
+
+    ft_is_negative(-1);
+    ft_is_negative(1);
+    printf("\n");
 
     return (0);
 }
