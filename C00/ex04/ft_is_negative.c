9c771ca4250b696d42a13699c97a2992db8ceed4
Gaeon <gaeon0413@gmail.com>
2022-05-18 18:39:47 +0900

    C00-ex04

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -6,7 +6,7 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 10:20:12 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/05/18 10:37:25 by gaeokim          ###   ########.fr       */
+/*   Updated: 2022/05/18 14:35:45 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,8 @@
 
 void	ft_is_negative(int n)
 {
-	char	negative;
-	char	positive;
-
-	negative = 'N';
-	positive = 'P';
 	if (n < 0)
-		write (1, &negative, 1);
+		write (1, "N", 1);
 	else
-		write (1, &positive, 1);
+		write (1, "P", 1);
 }
