c2edd70586c7b0998ce1fdcc758f2c95066815b0
Gaeon <gaeon0413@gmail.com>
2022-05-23 21:43:34 +0900

    C02

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 12:27:11 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/05/23 18:45:33 by gaeokim          ###   ########.fr       */
+/*   Updated: 2022/05/23 21:36:11 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,4 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 		dest[i] = '\0';
 	}
 	return (len);
-}
-
-int main()
-{
-	int i = 0 ;
-	int size = 5;
-	while(i < size - 1)
-	{
-		i++;
-	}
-	printf("%d", i);
 }
