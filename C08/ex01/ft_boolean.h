e775b6e70f56efe4d69c998fdffffe680b87a93a
Gaeon <gaeon0413@gmail.com>
2022-05-30 18:53:29 +0900

    C08

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/29 15:17:38 by gaeokim           #+#    #+#             */
+/*   Updated: 2022/05/29 15:28:18 by gaeokim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef int	t_bool;
+
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+# define EVEN(nbr) (nbr % 2 == 0)
+# define TRUE 1;
+# define FALSE 0;
+# define SUCCESS 0;
+
+#endif
