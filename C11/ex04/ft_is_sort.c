7d3e9b141d6a4d962cafcde9b97a96a9fa89f768
Gaeon <gaeon0413@gmail.com>
2022-06-04 20:02:55 +0900

    C11

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -6,7 +6,7 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 18:12:10 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/06/02 19:29:27 by gaeokim          ###   ########.fr       */
+/*   Updated: 2022/06/04 14:43:19 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,19 @@ int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 	int	res;
 
 	i = 0;
-	while (f(tab[i], tab[i + 1]) == 0)
+	while ((f(tab[i], tab[i + 1]) == 0) && (i + 1 < length))
 		i++;
-	res = f(tab[i], tab[i + 1]);
-	i++;
+	if (i + 1 < length)
+	{
+		res = f(tab[i], tab[i + 1]);
+		i++;
+	}
 	while (i + 1 < length)
 	{
-		if (res < 0)
-		{
-			if (f(tab[i], tab[i + 1]) > 0)
-				return (0);
-		}
-		else if (res > 0)
-		{
-			if (f(tab[i], tab[i + 1]) < 0)
-				return (0);
-		}
+		if ((res < 0) && (f(tab[i], tab[i + 1]) > 0))
+			return (0);
+		else if ((res > 0) && (f(tab[i], tab[i + 1]) < 0))
+			return (0);
 		i++;
 	}
 	return (1);
