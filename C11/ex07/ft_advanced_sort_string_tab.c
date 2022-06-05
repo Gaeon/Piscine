87f6712bdf4debf7199d5407f79cd077559e677a
Gaeon <gaeon0413@gmail.com>
2022-06-06 03:28:05 +0900

    C11

--- C11/ex07/ft_advanced_sort_string_tab.c
+++ C11/ex07/ft_advanced_sort_string_tab.c
@@ -6,32 +6,28 @@
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 11:28:48 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/06/06 00:47:54 by gaeokim          ###   ########.fr       */
+/*   Updated: 2022/06/06 03:27:46 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_swap1(char **str1, char **str2)
-{
-	char	*temp;
-
-	temp = *str1;
-	*str1 = *str2;
-	*str2 = temp;
-}	
-
 void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	char	*temp;
 
 	i = 0;
-	while (tab[i + 1] != 0)
+	while (tab[i])
 	{
-		j = i + 1;
-		while (tab[j] != 0)
+		j = 0;
+		while (tab[j] && tab[j + 1])
 		{
-			if (cmp(tab[i], tab[j]) > 0)
-				ft_swap1(&tab[i], &tab[j]);
+			if ((*cmp)(tab[j], tab[j + 1]) > 0)
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
 			j++;
 		}
 		i++;
