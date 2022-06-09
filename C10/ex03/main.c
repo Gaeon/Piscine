cc12947ccc71d40e0ec441fe53aba7789155ff75
Gaeon <gaeon0413@gmail.com>
2022-06-09 22:22:46 +0900

    C10

--- C10/ex03/main.c
+++ C10/ex03/main.c
@@ -5,44 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gaeokim <gaeokim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/06 17:26:20 by gaeokim           #+#    #+#             */
-/*   Updated: 2022/06/09 22:18:26 by gaeokim          ###   ########.fr       */
+/*   Created: 2022/06/09 22:16:33 by gaeokim           #+#    #+#             */
+/*   Updated: 2022/06/09 22:18:56 by gaeokim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <libgen.h>
 
-void	ft_display(int argc, char *argv[])
+int#pragma endregionmain(void)
 {
-	int		fd;
-	int		idx;
-	char	*c;
-
-	idx = 1;
-	while (idx < argc)
-	{
-		fd = open(argv[idx], O_RDONLY);
-		if (fd == -1)
-		{
-			exit(0);
-		}
-		else
-		{
-			while (read(fd, &c, 1))
-				write(1, &c, 1);
-			close(fd);
-		}
-		idx++;
-	}
-}
-
-int	main(int argc, char *argv[])
-{
-	if (argc < 2)
-		return (0);
-	else
-		ft_display(argc, argv);
+	write(1, "hi", 1);
 }
